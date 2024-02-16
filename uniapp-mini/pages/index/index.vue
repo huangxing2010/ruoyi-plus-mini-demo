fbe854b9940a0e447e9b188cfca57459ffa70123
huangxing123 <568363508@qq.com>
2024-02-16 12:22:49 +0800

    小程序样式调整

---
+++ uniapp-mini/pages/index/index.vue
@@ -0,0 +1,113 @@
+<template>
+	<view class="wrap">
+		<u-swiper :list="list"></u-swiper>
+
+		<view class="banner">
+			<u-grid :col="3">
+				<u-grid-item>
+					<u-icon name="photo" :size="46"></u-icon>
+					<view class="grid-text">图片</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="lock" :size="46"></u-icon>
+					<view class="grid-text">锁头</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="hourglass" :size="46"></u-icon>
+					<view class="grid-text">沙漏</view>
+				</u-grid-item>
+
+				<u-grid-item>
+					<u-icon name="lock" :size="46"></u-icon>
+					<view class="grid-text">锁头</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="hourglass" :size="46"></u-icon>
+					<view class="grid-text">沙漏</view>
+				</u-grid-item>
+				<u-grid-item>
+					<u-icon name="photo" :size="46"></u-icon>
+					<view class="grid-text">图片</view>
+				</u-grid-item>
+			</u-grid>
+		</view>
+
+		<u-card margin="12rpx">
+			<view class="" slot="head">
+				<u-section title="今日热门" sub-title="查看更多"></u-section>
+			</view>
+			<view class="" slot="body">
+				<view class="u-body-item u-flex u-border-bottom u-col-between u-p-t-0">
+					<view class="u-body-item-title u-line-2">瓶身描绘的牡丹一如你初妆，冉冉檀香透过窗心事我了然，宣纸上走笔至此搁一半</view>
+					<image
+						src="https://img11.360buyimg.com/n7/jfs/t1/94448/29/2734/524808/5dd4cc16E990dfb6b/59c256f85a8c3757.jpg"
+						mode="aspectFill"></image>
+				</view>
+				<view class="u-body-item u-flex u-row-between u-p-b-0">
+					<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+					<image
+						src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg"
+						mode="aspectFill"></image>
+				</view>
+			</view>
+
+		</u-card>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list: [{
+						image: 'https://cdn.uviewui.com/uview/swiper/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/2.jpg',
+						title: '身无彩凤双飞翼，心有灵犀一点通'
+					},
+					{
+						image: 'https://cdn.uviewui.com/uview/swiper/3.jpg',
+						title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳'
+					}
+				],
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.wrap {}
+
+	.grid-text {
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: $u-type-info;
+	}
+	
+	.banner{margin: 12rpx;}
+
+	// 卡片样式
+	.u-card-wrap {
+		background-color: $u-bg-color;
+		padding: 1px;
+	}
+
+	.u-body-item {
+		font-size: 32rpx;
+		color: #333;
+		padding: 20rpx 10rpx;
+	}
+
+	.u-body-item image {
+		width: 120rpx;
+		flex: 0 0 120rpx;
+		height: 120rpx;
+		border-radius: 8rpx;
+		margin-left: 12rpx;
+	}
+</style>
